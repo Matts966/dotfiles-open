2396f31d3cc12c80d8fef5272f15f547d7dc8662
Matts966 <Matts966@users.noreply.github.com>
2021-01-26 05:02:49 +0900

    Update todo.py

--- todo.py
+++ todo.py
@@ -1,45 +1,25 @@
+import os
 import json
 import requests
 import click
 from pyfzf import pyfzf
 import todoist
 from pprint import pprint
-import unicodedata
-
-
-def get_han_count(text):
-    count = 0
-    for char in text:
-        if unicodedata.east_asian_width(char) in "FWA":
-            count += 2
-        else:
-            count += 1
-    return count
-
-
-def text_align(text, width=40, *, align=-1, fill_char=" "):
-    fill_count = width - get_han_count(text)
-    if fill_count <= 0:
-        return text
-    if align < 0:
-        return text + fill_char * fill_count
-    else:
-        return fill_char * fill_count + text
 
 
 def format_item(item):
     due = item["due"]["string"] if item["due"] else ""
-    return "\t".join(map(text_align, [item["content"], due, str(item["id"])]))
+    return "{:<10}\t{:<10}\t{}".format(item["id"], due, item["content"])
 
 
 @click.command()
 @click.option("--filter", "-f", default="today|overdue")
 def todo(filter: str):
     d = json.JSONDecoder()
-    with open("/Users/matts966/.todoist.config.json") as config:
+    with open(
+        os.path.join(os.environ["HOME"], ".todoist.config.json")
+    ) as config:
         token = d.decode(config.read())["token"]
-
-    api = todoist.TodoistAPI(token)
 
     items = requests.get(
         "https://api.todoist.com/rest/v1/tasks",
@@ -48,13 +28,23 @@ def todo(filter: str):
     ).json()
     items = map(format_item, items)
     fzf = pyfzf.FzfPrompt()
+
+    print(fzf)
+
     try:
-        result = fzf.prompt(items, "--multi --cycle")
+        result = fzf.prompt(
+            items,
+            "--multi --cycle --preview 'curl --silent \
+                    https://api.todoist.com/sync/v8/items/get -d token="
+            + token
+            + " -d item_id=$(echo {} | cut -f1) | jq | bat --color=always'",
+        )
     except Exception:
         exit(1)
 
     pprint(result)
     print()
+    api = todoist.TodoistAPI(token)
     pprint(api.items.get_by_id(result[0].split()[-1]))
 
 
