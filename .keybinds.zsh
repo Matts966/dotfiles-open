bc20ef4309e00881248dc6770e7f280448252be7
Matts966 <Matts966@users.noreply.github.com>
2021-07-13 14:33:22 +0900

    Remove unused git keybinds

--- .keybinds.zsh
+++ .keybinds.zsh
@@ -7,16 +7,9 @@ WORDCHARS=${WORDCHARS/\/}
 # Ignore ctrl-d EOF and delete-char-or-list
 setopt ignore_eof
 
+
 # Git functions
 bindkey -r '^g'
-fbr() {
-  local branches branch
-  branches=$(git branch --all | grep -v HEAD)
-  branch=$(echo "$branches" | fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m)
-  git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
-}
-zle -N fbr
-bindkey '^g^b' fbr
 
 gcd() {
   repo=$(ghq list | fzf --preview "bat --color=always --style=header,grid --line-range :80 $(ghq root)/{}/README.*") && cd $(ghq root)/$repo
@@ -38,13 +31,6 @@ _lazygit() {
 zle -N _lazygit
 bindkey '^g^g' _lazygit
 
-fshow() {
-  git log --graph --all --color=always --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" | \
-    fzf --no-sort --tiebreak=index --bind=ctrl-s:toggle-sort \
-    --bind "ctrl-m:execute: (grep -o '[a-f0-9]\{7\}' | head -1 | xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF' {} FZF-EOF"
-  }
-zle -N fshow
-bindkey '^g^l' fshow
 
 function agg() {
   openvim='{system("$EDITOR " $1 " +" $2 " < /dev/tty > /dev/tty")}'
